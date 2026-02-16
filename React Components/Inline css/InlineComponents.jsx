74335676b013c2eb188b4a5faa8793622ce0aa12
H.M. Tahsin Sheikh <140962187+Tahsin22201243@users.noreply.github.com>
2026-02-16 10:49:52 +0600

    Inline CSS Component

---
+++ React Components/Inline css/InlineComponents.jsx
@@ -0,0 +1,4 @@
+const headerStyle = {color: "blue",fontSize: "140px"}
+export default function InlineComponents({ todo, toggleTodo }) {
+    return <div><h1 style={headerStyle}>Inline CSS</h1></div>
+}
